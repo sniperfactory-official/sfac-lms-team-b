2e1ad7c77e3d04686abe99589bc1b87753f1a0e7
Hun5LEE <jhlee091697@gmail.com>
2023-07-27 17:40:26 +0900

    Feat: user role이 수강생일때 submittedAssignment 기능구현

--- src/hooks/queries/useGetSubmittedAssignment.ts
+++ src/hooks/queries/useGetSubmittedAssignment.ts
@@ -13,36 +13,42 @@ import { SubmittedAssignment } from "@/types/firebase.types";
 
 const getSubmittedAssignments = async (
   assignmentId: string,
-): Promise<SubmittedAssignment[]> => {
-  // if (role === "수강생") {
-
-  // const qwe = getDocs(collection(db, "submittedAssignments"));
-  // const asd = (await qwe)?.docs.map((doc: any) => doc.data());
-  // return asd;
-  // }
-
-  // if (role === "관리자") {
-  const assignmentIdToQuery = "yjQmFY45VoZduNQ4Twxy";
-  const assignmentRef = doc(db, "assignments", assignmentIdToQuery);
+): Promise<SubmittedAssignment[] | SubmittedAssignment> => {
+  const assignmentRef = doc(db, "assignments", assignmentId);
   const assignmentSnapshot = await getDoc(assignmentRef);
 
-  const q = query(
+  // 추후 user정보를 전역상태로 관리 할 때 코드 변경 예정
+  const userRef = doc(db, "users", "userId");
+  const userSnapshot = await getDoc(userRef);
+
+  if ("수강생") {
+    const submittedAssignmentsQuery = query(
+      collection(db, "submittedAssignments"),
+      where("assignmentId", "==", assignmentSnapshot.ref),
+      where("userId", "==", userSnapshot.ref),
+    );
+
+    const submittedAssignmentsDocs = await getDocs(submittedAssignmentsQuery);
+    const submittedAssignments = submittedAssignmentsDocs?.docs.map(doc => {
+      return { id: doc.id, ...doc.data() } as SubmittedAssignment;
+    });
+
+    return submittedAssignments;
+  }
+
+  const submittedAssignmentsQuery = query(
     collection(db, "submittedAssignments"),
     where("assignmentId", "==", assignmentSnapshot.ref),
   );
-  const submittedAssignmentsDocs = await getDocs(q);
+  const submittedAssignmentsDocs = await getDocs(submittedAssignmentsQuery);
   const submittedAssignments = submittedAssignmentsDocs?.docs.map(doc => {
     return { id: doc.id, ...doc.data() } as SubmittedAssignment;
   });
 
   return submittedAssignments;
-
-  // }
-
-  // if(강사) { 해당assignment Id에 일치하는 모든 submittedAssignments 불러오기}
 };
+
 const useGetSubmittedAssignments = (assignmentId: string) => {
-  // 파라미터로 Role을 받아와 수강생이면 ~~, 강사면~~실행
   const { data, isLoading, error } = useQuery(
     ["getSubmittedAssignment", assignmentId],
     () => getSubmittedAssignments(assignmentId),
