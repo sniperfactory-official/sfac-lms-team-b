009281bcfbb4536a0cdb9d0e9f8fa61960aeb3e6
jeongdopark <jeongdo.dev@gmail.com>
2023-07-28 01:18:25 +0900

    Chore: firebase 연동 설정

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+ export { db };
+
