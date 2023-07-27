dd0f19fa4d95b7d25b3ad5f6d48ad28134e1be4b
leeyulgok <dbfrhr20@gmail.com>
2023-07-27 13:51:30 +0900

    Feat: DB에서 강의정보 가저와서 타입에 따른 랜더링 기능 구현

---
+++ src/components/lecture/typesOf/VideoLecture.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react";
+import type { LectureContent } from "@/types/firebase.Types";
+
+interface VideoLectureProps {
+  content: LectureContent;
+}
+
+const VideoLecture: FC<VideoLectureProps> = ({ content }) => {
+  const src = `https://www.youtube.com/embed/${content}`;
+
+  return (
+    <div className="videoContainer h-full w-full">
+      <iframe
+        src={src}
+        title="video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default VideoLecture;
