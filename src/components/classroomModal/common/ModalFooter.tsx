114863e93da5b1bdef56d8d7deadf1926f2f60f0
y-r-m <wkddpfla09@naver.com>
2023-07-27 23:43:55 +0900

    Feat: 강의 기간 설정 / 공개 여부 기능 구현 #58

--- src/components/classroomModal/common/ModalFooter.tsx
+++ src/components/classroomModal/common/ModalFooter.tsx
@@ -1,9 +1,10 @@
+import LectureSetting from "./LectureSetting";
 import ModalSubmitButton from "./ModalSubmitButton";
 
 const ModalFooter: React.FC = () => {
   return (
     <div className="flex justify-between mb-[-20px]">
-      {/* 이 위치에 수상 기간 및 강의 공개 컴포넌트  */}
+      <LectureSetting />
       <ModalSubmitButton />
     </div>
   );
