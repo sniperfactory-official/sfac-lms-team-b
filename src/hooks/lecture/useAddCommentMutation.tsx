1fa9ff0895bd6e54f97b76b619a72297e8d92824
leeyulgok <dbfrhr20@gmail.com>
2023-07-30 16:07:02 +0900

    Docs: 프리티어 적용

--- src/hooks/lecture/useAddCommentMutation.tsx
+++ src/hooks/lecture/useAddCommentMutation.tsx
@@ -1,6 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { db } from "@/utils/firebase";
-import { addDoc, collection, doc, serverTimestamp, DocumentData } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  DocumentData,
+} from "firebase/firestore";
 
 const addCommentToDB = async (data: {
   content: string;
@@ -32,20 +38,25 @@ export const useAddCommentMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation(addCommentToDB, {
-    onMutate: async (newComment) => {
-      await queryClient.cancelQueries(['comments']);
-      const previousComments = queryClient.getQueryData<DocumentData[]>(['comments']);
-      queryClient.setQueryData(['comments'], (old: DocumentData[] | undefined) => {
-        return [...(old ?? []), newComment];
-      });
+    onMutate: async newComment => {
+      await queryClient.cancelQueries(["comments"]);
+      const previousComments = queryClient.getQueryData<DocumentData[]>([
+        "comments",
+      ]);
+      queryClient.setQueryData(
+        ["comments"],
+        (old: DocumentData[] | undefined) => {
+          return [...(old ?? []), newComment];
+        },
+      );
       return { previousComments };
-    },    
+    },
     onSettled: () => {
-      queryClient.invalidateQueries(['comments']);
+      queryClient.invalidateQueries(["comments"]);
     },
     onError: (_err, _newComment, context) => {
       if (context?.previousComments) {
-        queryClient.setQueryData(['comments'], context.previousComments);
+        queryClient.setQueryData(["comments"], context.previousComments);
       }
     },
   });
