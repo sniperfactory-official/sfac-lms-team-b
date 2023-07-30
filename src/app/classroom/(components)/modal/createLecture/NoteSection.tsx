f27f59c4428336879a507c408b18cf56bcd9a826
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-30 23:56:00 +0900

    Style: 노트 생성 모달창의 마크다운 에디터에서 툴바 옵션 종류 수정 및 옵션의 위치 변경 #123

--- src/app/classroom/(components)/modal/createLecture/NoteSection.tsx
+++ src/app/classroom/(components)/modal/createLecture/NoteSection.tsx
@@ -1,14 +1,25 @@
+import { useRef } from "react";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 const NoteSction: React.FC = () => {
+  const editorRef = useRef<Editor>(null);
+
+  const toolbarItems = [
+    ["heading", "bold", "italic", "strike"],
+    ["image", "link"],
+    ["ul", "ol", "task", "table"],
+  ];
+
   return (
     <Editor
+      ref={editorRef}
       placeholder="내용을 입력해주세요."
       hideModeSwitch={true}
       usageStatistics={false}
       initialEditType="markdown"
       useCommandShortcut={true}
+      toolbarItems={toolbarItems}
     />
   );
 };
