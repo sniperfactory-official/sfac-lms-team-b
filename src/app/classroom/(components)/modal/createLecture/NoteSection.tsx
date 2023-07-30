fd26b0bec483adc03f8a95fa2e9aa37b9e17de40
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-31 02:23:02 +0900

    Feat: toast ui editor SSR 기능 해제 및 노트 생성 모달창 상태값 유지 #124

--- src/app/classroom/(components)/modal/createLecture/NoteSection.tsx
+++ src/app/classroom/(components)/modal/createLecture/NoteSection.tsx
@@ -1,9 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Editor } from "@toast-ui/react-editor";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { setLectureContent } from "@/redux/slice/lectureInfoSlice";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 const NoteSction: React.FC = () => {
   const editorRef = useRef<Editor>(null);
+  const dispatch = useDispatch();
+  const lectureContent = useSelector(
+    (state: RootState) => state.lectureInfo.lectureContent,
+  );
+  const [content, setContent] = useState<string | undefined>(lectureContent);
 
   const toolbarItems = [
     ["heading", "bold", "italic", "strike"],
@@ -11,15 +19,25 @@ const NoteSction: React.FC = () => {
     ["ul", "ol", "task", "table"],
   ];
 
+  const handleChangeContent = () => {
+    const newContent: string | undefined = editorRef.current
+      ?.getInstance()
+      .getMarkdown();
+    setContent(newContent);
+    dispatch(setLectureContent(newContent));
+  };
+
   return (
     <Editor
       ref={editorRef}
+      initialValue={content}
       placeholder="내용을 입력해주세요."
       hideModeSwitch={true}
       usageStatistics={false}
       initialEditType="markdown"
       useCommandShortcut={true}
       toolbarItems={toolbarItems}
+      onChange={handleChangeContent}
     />
   );
 };
