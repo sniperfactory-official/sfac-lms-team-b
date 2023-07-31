4fae9ab1418bf00996d3e3d2b6a6157e651b6c8d
Xeraph <jeongdo.dev@gmail.com>
2023-07-31 21:51:36 +0900

    Feat: 선택한 course 고유 ID데이터 저장 & 삭제

--- src/app/classroom/(components)/sidebar/SectionButton.tsx
+++ src/app/classroom/(components)/sidebar/SectionButton.tsx
@@ -1,23 +1,43 @@
+import useDeleteCourse from "@/hooks/mutation/useDeleteCourse";
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { handleEditMode } from "@/redux/slice/editCourseIdSlice";
+
 interface IProp {
   type: "set" | "remove";
-  onClick: () => void;
 }
 
-const SectionButton = ({ type, onClick }: IProp) => {
+const SectionButton = ({ type }: IProp) => {
+  const dispatch = useDispatch();
+  const { mutate: deleteCourse, isLoading } = useDeleteCourse();
+  const deleteIdArray = useSelector(
+    (state: RootState) => state.editCourse.deleteIdArray,
+  );
   const type_obj = {
     set: {
       text: "적용",
       bg: "bg-primary-80",
+      onClick: () => console.log("set"),
     },
     remove: {
       text: "선택 삭제",
       bg: "bg-red",
+      onClick: () =>
+        deleteCourse(
+          { deleteIdArray },
+          {
+            onSuccess: () => {
+              dispatch(handleEditMode());
+            },
+          },
+        ),
     },
   };
   return (
     <button
       className={`w-[115px] h-[35px] text-white ${type_obj[type].bg} rounded-lg text-[14px]`}
-      onClick={onClick}
+      onClick={type_obj[type].onClick}
     >
       {type_obj[type].text}
     </button>
