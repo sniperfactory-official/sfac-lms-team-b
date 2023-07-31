38b7444afbe9f445735399f9133da89cf6599f0d
Xeraph <jeongdo.dev@gmail.com>
2023-07-31 21:48:45 +0900

    Feat: 삭제할 course id 전역관리 slice 생성

--- src/redux/slice/editCourseIdSlice.ts
+++ src/redux/slice/editCourseIdSlice.ts
@@ -1,18 +1,18 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface DeleteId {
-  type : 'course' | 'lecture';
-  id : string
+  type: "course" | "lecture";
+  id: string;
 }
 
 export interface IEditCourse {
-  deleteIdArray : DeleteId[],
-  isEditMode : boolean
+  deleteIdArray: DeleteId[];
+  isEditMode: boolean;
 }
 
 const initialState: IEditCourse = {
-  deleteIdArray : [],
-  isEditMode : false,
+  deleteIdArray: [],
+  isEditMode: false,
 };
 
 const editCourseSlice = createSlice({
@@ -21,25 +21,29 @@ const editCourseSlice = createSlice({
   reducers: {
     toggleDeletionId: (state, action: PayloadAction<DeleteId>) => {
       const index = state.deleteIdArray.findIndex(
-        (item) => item.id === action.payload.id
+        item => item.id === action.payload.id,
       );
       if (index !== -1) {
         state.deleteIdArray.splice(index, 1);
       } else {
-        state.deleteIdArray.push({type: action.payload.type, id: action.payload.id});
+        state.deleteIdArray.push({
+          type: action.payload.type,
+          id: action.payload.id,
+        });
       }
     },
-    resetDeletionId: (state) => {
+    resetDeletionId: state => {
       state.deleteIdArray = [];
     },
-    handleEditMode: (state) => {
-      console.log('hello handleEditMOdeㅅ');
-      
-      state.isEditMode = !state.isEditMode
+    handleEditMode: state => {
+      console.log("hello handleEditMOdeㅅ");
+
+      state.isEditMode = !state.isEditMode;
       state.deleteIdArray = [];
-    }
+    },
   },
 });
 
-export const { toggleDeletionId, resetDeletionId, handleEditMode } = editCourseSlice.actions;
+export const { toggleDeletionId, resetDeletionId, handleEditMode } =
+  editCourseSlice.actions;
 export default editCourseSlice.reducer;
