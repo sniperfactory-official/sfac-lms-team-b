ba8609adca878679c0f7675e9b927faf7dd0a10b
Xeraph <jeongdo.dev@gmail.com>
2023-08-02 06:36:36 +0900

    Feat: Drag and Drop 순서변경 test ver

--- src/app/classroom/(components)/sidebar/CourseList.tsx
+++ src/app/classroom/(components)/sidebar/CourseList.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ICourseField, ILecture } from "@/hooks/queries/useGetCourseList";
 import Element from "./Element";
 import useSelectCourse from "@/hooks/classroom/useSelectCourse";
-import { useDispatch } from "react-redux";
-import { setCourseId } from "@/redux/slice/lectureInfoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { RootState } from "@/redux/store";
+import { DndProvider } from "react-dnd";
+import DndItem from "./DndItem";
 
 interface IProps {
   courseList: ICourseField[];
@@ -11,12 +14,27 @@ interface IProps {
 }
 
 const CourseList = ({ courseList, setCurrentCourse }: IProps) => {
+  const isEditMode = useSelector(
+    (state: RootState) => state.editCourse.isEditMode,
+  );
   const dispatch = useDispatch();
-  const { selectedCourse, handleCurrentCourse } = useSelectCourse({
-    courseList,
-    setCurrentCourse,
-  });
-  // fix to merge
+  const selectedCourse = useSelector(
+    (state: RootState) => state.editCourse.selectedCourse,
+  );
+  const { handleCurrentCourse, currentLectures, setCurrentLectures } =
+    useSelectCourse({
+      courseList,
+      setCurrentCourse,
+    });
+
+  const moveItem = (dragIndex: number, hoverIndex: number) => {
+    const dragItem = currentLectures[dragIndex];
+    const newList = [...currentLectures];
+    newList.splice(dragIndex, 1);
+    newList.splice(hoverIndex, 0, dragItem);
+    setCurrentLectures(newList);
+  };
+
   return (
     // courseFiled 데이터 구조
     // courseData : {title: 'IT기본', createdAt: Timestamp, updatedAt: Timestamp}
@@ -37,14 +55,37 @@ const CourseList = ({ courseList, setCurrentCourse }: IProps) => {
           />
           {/* 선택된 lecture만 보이도록 */}
           {selectedCourse[idx] &&
-            course.lectureList.map((lecture: ILecture) => (
-              <Element
-                key={lecture.lectureId}
-                type="lecture"
-                title={lecture.title}
-                isSelected={selectedCourse[idx]}
-                uniqueId={lecture.lectureId}
-              />
+            (isEditMode ? (
+              <DndProvider backend={HTML5Backend}>
+                {currentLectures.map((lecture: ILecture, index: number) => (
+                  <DndItem
+                    key={lecture.id}
+                    index={index}
+                    id={lecture.lectureId}
+                    moveItem={(dragIndex, hoverIndex) =>
+                      moveItem(dragIndex, hoverIndex)
+                    }
+                    child={
+                      <Element
+                        type="lecture"
+                        title={lecture.title}
+                        isSelected={selectedCourse[idx]}
+                        uniqueId={lecture.lectureId}
+                      />
+                    }
+                  />
+                ))}
+              </DndProvider>
+            ) : (
+              course.lectureList.map((lecture: ILecture) => (
+                <Element
+                  key={lecture.lectureId}
+                  type="lecture"
+                  title={lecture.title}
+                  isSelected={selectedCourse[idx]}
+                  uniqueId={lecture.lectureId}
+                />
+              ))
             ))}
         </>
       ))}
