cdba9abe32efed1a740c4040ff4b028efe9b1fb8
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-01 18:00:35 +0900

    Update: 강의 생성시 수강기간 관련 코드 변수 변경 및 코드 정리

--- src/app/classroom/(components)/modal/common/LectureSetting.tsx
+++ src/app/classroom/(components)/modal/common/LectureSetting.tsx
@@ -1,32 +1,25 @@
+import { useDispatch } from "react-redux";
+import { Timestamp } from "firebase/firestore";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/locale";
 import {
   setStartDate,
   setEndDate,
-  setIsLecturePublic,
+  setIsLecturePrivate,
 } from "@/redux/slice/lectureInfoSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
-import { Timestamp } from "firebase/firestore";
+import useLectureInfo from "@/hooks/lecture/useLectureInfo";
 
 const LectureSetting: React.FC = () => {
-  const startDate = useSelector(
-    (state: RootState) => state.lectureInfo.startDate,
-  );
-  const endDate = useSelector((state: RootState) => state.lectureInfo.endDate);
-  const isLecturePublic = useSelector(
-    (state: RootState) => state.lectureInfo.isLecturePublic,
-  );
-
   const dispatch = useDispatch();
+  const { startDate, endDate, isLecturePrivate } = useLectureInfo();
 
   const handleToggle = () => {
-    dispatch(setIsLecturePublic(!isLecturePublic));
+    dispatch(setIsLecturePrivate(!isLecturePrivate));
   };
 
-  const handleChangeDate = (update: [Date, Date]) => {
-    const [startDate, endDate] = update;
+  const handleChangeDate = (ranges: [Date, Date]) => {
+    const [startDate, endDate] = ranges;
     dispatch(
       setStartDate(
         startDate ? new Timestamp(startDate.getTime() / 1000, 0) : null,
@@ -51,9 +44,10 @@ const LectureSetting: React.FC = () => {
           placeholderText="Pick a date"
           locale={ko}
           selected={startDate ? timestampToDate(startDate) : null}
-          onChange={handleChangeDate}
           startDate={startDate ? timestampToDate(startDate) : null}
           endDate={endDate ? timestampToDate(endDate) : null}
+          onChange={handleChangeDate}
+          minDate={new Date()}
           selectsRange
           className="bg-white border-2 border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 cursor-pointer"
           dateFormat="yyyy.MM.dd"
@@ -69,7 +63,7 @@ const LectureSetting: React.FC = () => {
             id="toggle"
             name="toggle"
             className="hidden"
-            checked={isLecturePublic}
+            checked={isLecturePrivate}
             onChange={handleToggle}
           />
           <div
@@ -80,20 +74,20 @@ const LectureSetting: React.FC = () => {
               width: "100%",
               height: "100%",
               borderRadius: "26px",
-              backgroundColor: isLecturePublic ? "#e5eeff" : "#f2f2f2",
+              backgroundColor: isLecturePrivate ? "#f2f2f2" : "#e5eeff",
               transition: "all 0.4s ease-in-out",
             }}
-          ></div>
+          />
           <div
             className="absolute top-50%"
             style={{
               position: "absolute",
               top: "50%",
-              left: isLecturePublic ? "calc(100% - 21px)" : "5px",
+              left: isLecturePrivate ? "5px" : "calc(100% - 21px)",
               width: "16px",
               height: "16px",
               borderRadius: "13px",
-              backgroundColor: isLecturePublic ? "#337aff" : "#c5c5c5",
+              backgroundColor: isLecturePrivate ? "#c5c5c5" : "#337aff",
               boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.3)",
               transition: "all 0.4s ease-in-out",
               transform: "translateY(-50%)",
