99a9aaf1d49a2e687017e6c087a82ac1e5f9f6a8
Xeraph <jeongdo.dev@gmail.com>
2023-08-03 01:00:59 +0900

    Feat: Sidebar 섹션에 커스텀 훅 적용

--- src/app/classroom/(components)/Sidebar.tsx
+++ src/app/classroom/(components)/Sidebar.tsx
@@ -3,30 +3,70 @@ import { ICourseField } from "@/hooks/queries/useGetCourseList";
 import SectionHandlerButton from "./sidebar/SectionHandlerButton";
 import EditButton from "./sidebar/EditButton";
 import useCreateCourseMutation from "@/hooks/mutation/useCreateCourseMutation";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
+import useClickOutside from "@/hooks/classroom/useClickOutside";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import {
+  handleEditMode,
+  setCurrentLecture,
+} from "@/redux/slice/editCourseIdSlice";
 
 interface IProps {
   courseList: ICourseField[];
   setCurrentCourse: React.Dispatch<React.SetStateAction<any>>;
 }
 
-const Sidebar = ({ courseList, setCurrentCourse }: IProps) => {
-  const { mutate: createCourse, isLoading } = useCreateCourseMutation();
-  const handleCreateSection = () => {
-    createCourse();
-    setCurrentCourse(courseList![0]);
-  };
+const Sidebar = forwardRef<HTMLDivElement, IProps>(
+  ({ courseList, setCurrentCourse }, ref) => {
+    const { mutate: createCourse, isLoading } = useCreateCourseMutation();
+    const [getBackLectureOrderTrigger, setGetBackLectureOrderTrigger] =
+      useState<boolean>(false);
+    const handleCreateSection = () => {
+      createCourse();
+      setCurrentCourse(courseList![0]);
+    };
 
-  return (
-    <aside className="w-1/5 h-100 flex items-center flex-col mr-[20px] pt-[50px]">
-      <CourseList courseList={courseList} setCurrentCourse={setCurrentCourse} />
-      <SectionHandlerButton
-        text="섹션 추가"
-        src="/images/plus.svg"
-        onClick={handleCreateSection}
-      />
-      <EditButton />
-    </aside>
-  );
-};
+    const dispatch = useDispatch();
+    const isEditMode = useSelector(
+      (state: RootState) => state.editCourse.isEditMode,
+    );
+    const selectedCourse = useSelector(
+      (state: RootState) => state.editCourse.selectedCourse,
+    );
+
+    useEffect(() => {
+      const index = selectedCourse.findIndex(value => value === true);
+      dispatch(setCurrentLecture(courseList[index].lectureList));
+    }, [getBackLectureOrderTrigger]);
+
+    const sidebarRef = useRef<HTMLDivElement>(null);
+    useClickOutside(
+      sidebarRef,
+      () => dispatch(handleEditMode()),
+      isEditMode,
+      setGetBackLectureOrderTrigger,
+      getBackLectureOrderTrigger,
+    );
+
+    return (
+      <aside
+        ref={sidebarRef}
+        className="w-1/5 h-100 flex items-center flex-col mr-[20px] pt-[50px]"
+      >
+        <CourseList
+          courseList={courseList}
+          setCurrentCourse={setCurrentCourse}
+        />
+        <SectionHandlerButton
+          text="섹션 추가"
+          src="/images/plus.svg"
+          onClick={handleCreateSection}
+        />
+        <EditButton />
+      </aside>
+    );
+  },
+);
 
 export default Sidebar;
