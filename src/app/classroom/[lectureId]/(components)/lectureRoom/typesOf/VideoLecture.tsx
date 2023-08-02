ca4b2677b8f6b7ac8d82fc53083d101b908d1058
leeyulgok <dbfrhr20@gmail.com>
2023-08-02 18:34:17 +0900

    Feat: 비디오 강의 기능 구현

--- src/app/classroom/[lectureId]/(components)/lectureRoom/typesOf/VideoLecture.tsx
+++ src/app/classroom/[lectureId]/(components)/lectureRoom/typesOf/VideoLecture.tsx
@@ -1,22 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import type { LectureContent } from "@/types/firebase.types";
+import { getMediaURL } from "@/hooks/lecture/useGetMediaURL";
 
 interface VideoLectureProps {
   content: LectureContent;
 }
 
 const VideoLecture: FC<VideoLectureProps> = ({ content }) => {
-  const src = `https://www.youtube.com/embed/${content}`;
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    getMediaURL(content.videoUrl).then(setVideoUrl).catch(console.error);
+  }, [content.videoUrl]);
 
   return (
     <div className="videoContainer h-full w-full">
-      <iframe
-        src={src}
-        title="video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        className="w-full h-full"
-      />
+      {videoUrl ? (
+        <video
+          src={videoUrl}
+          title="video player"
+          className="w-full h-full"
+          autoPlay
+          controls
+          muted
+          playsInline
+        />
+      ) : (
+        <div>비디오를 불러올 수 없습니다.</div>
+      )}
     </div>
   );
 };
