b9ace6ccac8669b345bb846e906d83b5393736b6
Xeraph <jeongdo.dev@gmail.com>
2023-08-03 01:01:13 +0900

    Chore: husky

--- src/hooks/classroom/useClickOutside.ts
+++ src/hooks/classroom/useClickOutside.ts
@@ -1,21 +1,28 @@
-import { RefObject, useEffect, useCallback } from 'react';
+import { RefObject, useEffect, useCallback } from "react";
 
-const useClickOutside = (ref: RefObject<HTMLElement>, callback: () => void, when: boolean, setGetBackLectureOrderTrigger: React.Dispatch<React.SetStateAction<boolean>>, getBackLectureOrderTrigger:boolean): void => {
-  const handleClickOutside = useCallback((event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as Node)) {
-      console.log('outside click');
-      setGetBackLectureOrderTrigger(!getBackLectureOrderTrigger)
-      callback();
-    }
-  }, [ref, callback]);
+const useClickOutside = (
+  ref: RefObject<HTMLElement>,
+  callback: () => void,
+  when: boolean,
+  setGetBackLectureOrderTrigger: React.Dispatch<React.SetStateAction<boolean>>,
+  getBackLectureOrderTrigger: boolean,
+): void => {
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        console.log("outside click");
+        setGetBackLectureOrderTrigger(!getBackLectureOrderTrigger);
+        callback();
+      }
+    },
+    [ref, callback],
+  );
 
   useEffect(() => {
     if (when) {
-      
-      
-      document.addEventListener('click', handleClickOutside);
+      document.addEventListener("click", handleClickOutside);
       return () => {
-        document.removeEventListener('click', handleClickOutside);
+        document.removeEventListener("click", handleClickOutside);
       };
     }
   }, [when, handleClickOutside]);
