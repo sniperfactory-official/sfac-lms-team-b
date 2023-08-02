07f40a9bb9e991de1c2ea4755bc1d97116e9bb8d
Yuni0221 <moonrise002002@gmail.com>
2023-08-02 14:18:21 +0900

    Update: 불필요한 import 제거

--- src/app/assignment/(components)/AssignmentCreate.tsx
+++ src/app/assignment/(components)/AssignmentCreate.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { Assignment } from "@/types/firebase.types";
 import PageToast from "@/components/PageToast";
 import { useCreateAssignment } from "@/hooks/mutation/useCreateAssignment";
-// import { User } from "firebase/auth";
 
 interface AssignmentCreateProps {
   isOpen: boolean;
