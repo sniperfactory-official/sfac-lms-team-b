2b2b0413ec673c9ac96df39944b960f984250db3
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-04 01:32:15 +0900

    Feat: 모달창의 역할에 따라 모달창 제목을 다르게 띄우도록 구현

--- src/app/classroom/(components)/modal/createLecture/AddLinkModal.tsx
+++ src/app/classroom/(components)/modal/createLecture/AddLinkModal.tsx
@@ -9,12 +9,13 @@ import useLectureInfo from "@/hooks/lecture/useLectureInfo";
 
 const AddLinkModal: React.FC = () => {
   const dispatch = useDispatch();
-  const { handleModalMove } = useClassroomModal();
+  const { modalRole, handleModalMove } = useClassroomModal();
   const { externalLink } = useLectureInfo();
-
-  const prevModal = () => {
-    handleModalMove("lectureTypeModalOpen", "linkModalOpen");
+  const MODAL_ROLE_OBJ: { [key: string]: string } = {
+    create: "링크 만들기",
+    edit: "수정하기",
   };
+
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputContent = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,13 +40,18 @@ const AddLinkModal: React.FC = () => {
 
   return (
     <Layout>
-      <ModalHeader currentModalName={"링크 만들기"}>
-        <button
-          onClick={prevModal}
-          className="text-xl font-bold text-grayscale-100"
-        >
-          강의 만들기
-        </button>
+      <ModalHeader currentModalName={MODAL_ROLE_OBJ[modalRole]}>
+        {modalRole === "create" ? (
+          <button
+            onClick={() =>
+              handleModalMove("lectureTypeModalOpen", "linkModalOpen")
+            }
+          >
+            강의 만들기
+          </button>
+        ) : (
+          <span>강의 수정</span>
+        )}
       </ModalHeader>
       <ModalMain>
         <label htmlFor="externalLink" className="hidden" />
