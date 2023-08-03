76c8c9b938e08c114ba41c38669c04f0c2bf7657
Xeraph <jeongdo.dev@gmail.com>
2023-08-03 19:37:23 +0900

    Chore: husky

--- src/app/classroom/(components)/main/ContentCard.tsx
+++ src/app/classroom/(components)/main/ContentCard.tsx
@@ -2,34 +2,34 @@ import { ILecture } from "@/hooks/queries/useGetCourseList";
 import { useRouter } from "next/navigation";
 import timestampToDate from "@/utils/timestampToDate";
 import Image from "next/image";
-import thumnail from "../../../../../public/images/thumnail.png"
+import thumnail from "../../../../../public/images/thumnail.png";
 const ContentCard = ({ lecture }: { lecture: ILecture }) => {
   const router = useRouter();
   const handleMovePage = () => {
     router.push(`/classroom/${lecture.lectureId}`);
   };
   console.log(lecture);
-  const {title,  lectureType, isPrivate, startDate, endDate, lectureContent} = lecture
-  const [start, end] = [timestampToDate(startDate), timestampToDate(endDate)]
+  const { title, lectureType, isPrivate, startDate, endDate, lectureContent } =
+    lecture;
+  const [start, end] = [timestampToDate(startDate), timestampToDate(endDate)];
 
   const LECTURE_OBJ = {
     노트: {
-      emoji : "📒",
-      text : "노트",
-      time : ""
+      emoji: "📒",
+      text: "노트",
+      time: "",
     },
     비디오: {
-      emoji : "🎬",
-      text : "비디오",
-      time : '10분',
+      emoji: "🎬",
+      text: "비디오",
+      time: "10분",
     },
     링크: {
-      emoji : "🔗",
-      text : "링크",
-      time : ""
-    }
-
-  }
+      emoji: "🔗",
+      text: "링크",
+      time: "",
+    },
+  };
   // console.log(lectureContent.images!.length);
 
   return (
@@ -39,7 +39,7 @@ const ContentCard = ({ lecture }: { lecture: ILecture }) => {
         <Image src={lectureContent.images![0]} alt="thumbnail" width={100} height={100}/>
       </div> :  */}
       <div className="w-1/3 h-5/6 rounded-lg mr-[25px]">
-        <Image src={thumnail} alt="thumnail" placeholder="blur"/>
+        <Image src={thumnail} alt="thumnail" placeholder="blur" />
       </div>
       <div className="w-2/3 h-5/6 ml-20px flex flex-col">
         <div className="text-xs ml-auto flex items-center w-[60px] text-grayscale-100 justify-around text-[12px]">
@@ -47,13 +47,20 @@ const ContentCard = ({ lecture }: { lecture: ILecture }) => {
           <div className="w-[0.5px] h-3 border-[0.5px] border-black"></div>
           <button className="text-xs">삭제</button>
         </div>
-        {lectureType === "비디오" && <div className="bg-grayscale-5 rounded w-[40px] h-[20px] text-xs text-center leading-[20px] mb-[10px] text-grayscale-60">
-          {LECTURE_OBJ[lectureType].time}</div>}
-        <div className="font-bold mb-[10px]">{LECTURE_OBJ[lectureType].emoji} {lecture.title}</div>
+        {lectureType === "비디오" && (
+          <div className="bg-grayscale-5 rounded w-[40px] h-[20px] text-xs text-center leading-[20px] mb-[10px] text-grayscale-60">
+            {LECTURE_OBJ[lectureType].time}
+          </div>
+        )}
+        <div className="font-bold mb-[10px]">
+          {LECTURE_OBJ[lectureType].emoji} {lecture.title}
+        </div>
         <div className="flex flex-row justify-between mt-[20px]">
           <div className="flex flex-col">
             <div className="text-xs">[수강기간]</div>
-            <div className="text-xs">{start}-{end}</div>
+            <div className="text-xs">
+              {start}-{end}
+            </div>
           </div>
           <button
             className="w-[140px] h-[35px] bg-grayscale-5 text-center leading-[35px] text-sm rounded-lg"
