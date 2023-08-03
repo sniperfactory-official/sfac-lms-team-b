90ecd02c218b409c41221cd1d658f31cd7a038ca
leeyulgok <dbfrhr20@gmail.com>
2023-08-03 23:34:20 +0900

    Update: profileImage가 없어도 기본 이미지 추가

--- src/app/classroom/(components)/modal/comment/UserImage.tsx
+++ src/app/classroom/(components)/modal/comment/UserImage.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState, useEffect } from "react";
 import Image from "next/image";
 import { getMediaURL } from "@/hooks/lecture/useGetMediaURL";
+import thumbnail from "../../../../../../public/images/thumnail.png";
 
 interface UserImageProps {
   profileImage?: string | null;
@@ -18,20 +19,16 @@ const UserImage: FC<UserImageProps> = ({ profileImage, size = "default" }) => {
 
   const wrapperClass = size === "default" ? "w-7 h-7" : "w-10 h-10";
 
-  return profileImageURL ? (
+  return (
     <div className={`${wrapperClass} relative rounded-full flex-shrink-0`}>
       <Image
-        src={profileImageURL}
+        src={profileImageURL || thumbnail}
         alt="사용자 이미지"
         sizes="(max-width: 768px) 100vw"
         fill={true}
         className="rounded-full"
       />
     </div>
-  ) : (
-    <div
-      className={`${wrapperClass} relative bg-white border border-gray-300 rounded-full flex-shrink-0`}
-    ></div>
   );
 };
 
