c81c47197965fd825b0dd084257b9765d7af6cf8
leeyulgok <dbfrhr20@gmail.com>
2023-08-04 17:14:32 +0900

    Refactor: 유저이미지 함수와 관련하여 중복 수정 및 리팩토링

--- src/app/classroom/(components)/modal/comment/UserImage.tsx
+++ src/app/classroom/(components)/modal/comment/UserImage.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC } from "react";
 import Image from "next/image";
-import { getMediaURL } from "@/hooks/lecture/useGetMediaURL";
 import thumbnail from "../../../../../../public/images/thumnail.png";
 
 interface UserImageProps {
@@ -9,20 +8,12 @@ interface UserImageProps {
 }
 
 const UserImage: FC<UserImageProps> = ({ profileImage, size = "default" }) => {
-  const [profileImageURL, setProfileImageURL] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (profileImage) {
-      getMediaURL(profileImage).then(setProfileImageURL).catch(console.error);
-    }
-  }, [profileImage]);
-
   const wrapperClass = size === "default" ? "w-7 h-7" : "w-10 h-10";
 
   return (
     <div className={`${wrapperClass} relative rounded-full flex-shrink-0`}>
       <Image
-        src={profileImageURL || thumbnail}
+        src={profileImage || thumbnail}
         alt="사용자 이미지"
         sizes="(max-width: 768px) 100vw"
         fill={true}
--- src/app/classroom/(components)/modal/comment/UserInfo.tsx
+++ src/app/classroom/(components)/modal/comment/UserInfo.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 
 interface UserInfoProps {
-  username: string;
+  username: string | undefined;
   role?: string;
   isForm?: boolean;
+  isHeader?: boolean;
 }
 
-const UserInfo: React.FC<UserInfoProps> = ({ username, role, isForm }) => (
-  <div className="flex items-center mb-1">
-    <span className={isForm ? "" : "font-semibold "}>{username}</span>
-    <span className="text-sm ml-1 text-gray-500 font-light">&#183; {role}</span>
-  </div>
-);
-
+const UserInfo: React.FC<UserInfoProps> = ({
+  username,
+  role,
+  isForm,
+  isHeader,
+}) => {
+  return isHeader ? (
+    <div className="flex items-center ml-2">
+      <span className=" text-sm font-semibold text-blue-500 ">{username}</span>
+      <span className="text-sm ml-1 text-gray-500">&#183; {role}</span>
+    </div>
+  ) : (
+    <div className="flex items-center mb-1">
+      <span className={isForm ? "" : "font-semibold "}>{username}</span>
+      <span className="text-sm ml-1 text-gray-500 font-light">
+        &#183; {role}
+      </span>
+    </div>
+  );
+};
 export default UserInfo;
