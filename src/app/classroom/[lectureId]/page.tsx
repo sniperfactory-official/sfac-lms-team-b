9ac1e5a23536516799fe240b6e62cb0343a98b58
leeyulgok <dbfrhr20@gmail.com>
2023-08-06 13:26:34 +0900

    Refactor: 강의 조회 함수 통합

--- src/app/classroom/[lectureId]/page.tsx
+++ src/app/classroom/[lectureId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import useGetLectureInfo from "@/hooks/queries/useGetLectureInfo";
 
 import LectureHeader from "@/app/classroom/[lectureId]/(components)/lectureRoom/LectureHeader";
 import TypeOfLecture from "@/app/classroom/[lectureId]/(components)/lectureRoom/typesOf/TypeOfLecture";
@@ -9,12 +10,18 @@ import LectureNavigation from "@/app/classroom/[lectureId]/(components)/lectureR
 
 const LectureHome = ({ params }: { params: { lectureId: string } }) => {
   const { lectureId } = params;
+  const { data: lectureData, isFetching } = useGetLectureInfo(lectureId);
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <main className="lectuerContainer flex flex-col w-full h-full">
-      <LectureHeader lectureId={lectureId} />
+      <LectureHeader lectureData={lectureData} />
       <div className="mainContainer flex w-full h-screen">
         <div className="Container w-3/4 flex flex-col h-screen">
-          <TypeOfLecture lectureId={lectureId} />
+          <TypeOfLecture lectureData={lectureData} />
           <LectureNavigation lectureId={lectureId} />
         </div>
         <LectureComment lectureId={lectureId} />
