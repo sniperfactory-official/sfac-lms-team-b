c1b3a6c004f8170ebe0e1ffcfcd8bdce158b428a
leeyulgok <dbfrhr20@gmail.com>
2023-08-09 19:40:24 +0900

    Update: 업데이트 된 함수에 맞춰 컴포넌트 수정

--- src/app/classroom/[lectureId]/(components)/lectureRoom/typesOf/VideoLecture.tsx
+++ src/app/classroom/[lectureId]/(components)/lectureRoom/typesOf/VideoLecture.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
+import { useLectureProgress } from "@/hooks/lecture/useLectureProgress";
+import { useLectureCompletion } from "@/hooks/lecture/useLectureCompletion";
 import type { LectureContent, Progress } from "@/types/firebase.types";
 import ReactPlayer from "react-player";
 
@@ -9,17 +11,43 @@ interface VideoLectureProps {
 
 const VideoLecture: FC<VideoLectureProps> = ({ content, progressInfo }) => {
   const { videoUrl } = content;
-  console.log(progressInfo);
-  const controls = progressInfo?.isCompleted !== false;
+  const videoRef = useRef<any>(null);
+  const [duration, setDuration] = useState<number>(0);
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  const { handleProgress } = useLectureProgress(
+    progressInfo?.id || "",
+    duration,
+  );
+
+  useLectureCompletion(progressInfo?.id || "", isCompleted);
+
+  const handleDuration = (duration: number) => {
+    setDuration(duration);
+  };
+
+  const handleEnded = () => {
+    setIsCompleted(true);
+  };
+
+  useEffect(() => {
+    if (progressInfo?.playTimes[0]?.start) {
+      videoRef.current.seekTo(Number(progressInfo.playTimes[0].start));
+    }
+  }, [progressInfo]);
 
   return (
     <div className="videoContainer h-full w-full">
       {videoUrl ? (
         <ReactPlayer
+          ref={videoRef}
           url={videoUrl}
           width="100%"
           height="100%"
-          controls={controls}
+          controls
+          onProgress={handleProgress}
+          onDuration={handleDuration}
+          onEnded={handleEnded}
         />
       ) : (
         <div>비디오를 불러올 수 없습니다.</div>
