a5a5eacc1559a92719167da0dd765a194146d65c
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-19 01:30:49 +0900

    Fix: 영상 강의 모달창에서 강의 수정 중이던 비디오파일이 나타나는 현상 수정

--- src/app/classroom/(components)/modal/common/Layout.tsx
+++ src/app/classroom/(components)/modal/common/Layout.tsx
@@ -4,6 +4,7 @@ import { closeModal } from "@/redux/slice/classroomModalSlice";
 import Image from "next/image";
 import { resetInput } from "@/redux/slice/lectureInfoSlice";
 import { resetDropzone } from "@/redux/slice/dropzoneFileSlice";
+import useClassroomModal from "@/hooks/lecture/useClassroomModal";
 
 interface ModalProps {
   children: ReactNode;
@@ -11,11 +12,15 @@ interface ModalProps {
 
 const Layout: React.FC<ModalProps> = ({ children }) => {
   const dispatch = useDispatch();
+  const { modalRole } = useClassroomModal();
 
   return (
     <div
       className="fixed top-0 left-0 w-screen h-screen bg-black/30 flex flex-col justify-center items-center"
-      onClick={() => dispatch(closeModal())}
+      onClick={() => {
+        modalRole === "edit" && dispatch(resetDropzone());
+        dispatch(closeModal());
+      }}
     >
       <article
         className="relative w-[770px] bg-white px-[35px] py-[40px] flex flex-col gap-5 rounded-[10px] border border-solid border-grayscale-10 drop-shadow-[0_0_8px_rgba(0,0,0,0.25)] box-border"
